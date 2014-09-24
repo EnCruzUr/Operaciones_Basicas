5f8577dc3cff781eb9fef9e392e37802a209cbf0
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-24 00:10:25 -0500

    Limpieza
    Se eliminó código innecesario.

--- Operaciones_Basicas/Program.cs
+++ Operaciones_Basicas/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Operaciones_Basicas
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
