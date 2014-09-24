6ed8c732d6cd8bd817450a1863da889592b559a0
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-24 00:22:23 -0500

    Menu
    El Menu del programa funcionando

--- Operaciones_Basicas/Program.cs
+++ Operaciones_Basicas/Program.cs
@@ -4,6 +4,32 @@ namespace Operaciones_Basicas
     class Program
     {
         public static void Menu(){
+            int Opcion;
+            Console.WriteLine("Seleccione Operacion que desea realizar");
+            Console.WriteLine("1)Suma");
+            Console.WriteLine("2)Resta");
+            Console.WriteLine("3)Multiplicacion");
+            Console.WriteLine("4)Division");
+            Console.WriteLine("5)Salir");
+            Opcion=int.Parse(Console.ReadLine());
+            switch(Opcion){
+                case 1:
+                    Suma();
+                    break;
+                case 2:
+                    Res();
+                    break;
+                case 3:
+                    Mult();
+                    break;
+                case 4:
+                    Div();
+                    break;
+                case 5:
+                    Console.Write("\n\nPresione una tecla para salir . . . ");
+			        Console.ReadKey(true);
+                    break;
+            }
 
         }
         public static void Suma()
@@ -25,7 +51,7 @@ namespace Operaciones_Basicas
 
         static void Main(string[] args)
         {
-
+            Menu();
         }
     }
 }
