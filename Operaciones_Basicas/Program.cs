7d05e93110d58441d602b79e7ef458eb30a66b1d
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-24 00:13:06 -0500

    Clases
    Creacion de las clases necesarias para el funcionamiento del proyecto.

--- Operaciones_Basicas/Program.cs
+++ Operaciones_Basicas/Program.cs
@@ -3,6 +3,26 @@ namespace Operaciones_Basicas
 {
     class Program
     {
+        public static void Menu(){
+
+        }
+        public static void Suma()
+        {
+
+        }
+        public static void Res()
+        {
+
+        }
+        public static void Mult()
+        {
+
+        }
+        public static void Div()
+        {
+
+        }
+
         static void Main(string[] args)
         {
 
